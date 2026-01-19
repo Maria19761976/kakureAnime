eec7971541636ff005d3504a8b25336aa7179cc2
David-Navarro-Oliver <navarropalle@gmail.com>
2026-01-19 14:29:43 +0100

    feat(navbar): add basic navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,63 @@
 import { NavLink } from "react-router-dom";
 
+const linkBase = "text-sm font-semibold";
+const linkInactive = "opacity-70 hover:opacity-100";
+const linkActive = "opacity-100 underline";
+
 export default function Navbar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>{" | "}
-      <NavLink to="/movies">Movies</NavLink>
-    </nav>
+    <header>
+      <nav
+        style={{
+          display: "flex",
+          gap: "16px",
+          alignItems: "center",
+          padding: "12px 16px",
+          borderBottom: "1px solid #e5e7eb",
+        }}
+      >
+        <NavLink to="/" className={`${linkBase} ${linkInactive}`}>
+          Kakure Anime
+        </NavLink>
+
+        <div style={{ display: "flex", gap: "12px", marginLeft: "12px" }}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${linkBase} ${isActive ? linkActive : linkInactive}`
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/movies"
+            className={({ isActive }) =>
+              `${linkBase} ${isActive ? linkActive : linkInactive}`
+            }
+          >
+            Movies
+          </NavLink>
+
+          <NavLink
+            to="/add-movie"
+            className={({ isActive }) =>
+              `${linkBase} ${isActive ? linkActive : linkInactive}`
+            }
+          >
+            Add
+          </NavLink>
+
+          <NavLink
+            to="/location"
+            className={({ isActive }) =>
+              `${linkBase} ${isActive ? linkActive : linkInactive}`
+            }
+          >
+            Location
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   );
 }
